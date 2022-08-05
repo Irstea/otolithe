1ac934499a81a946b6aa35901326a921e629bad5
Eric Quinton <eric.quinton@inrae.fr>
2022-08-05 17:30:12 +0200

    :fix install: correction post installation en prod

--- install/pgsql/alter-2.3-2.4.sql
+++ install/pgsql/alter-2.3-2.4.sql
@@ -1,4 +1,4 @@
-drop table login_oldpassword cascade;
+drop table if exists login_oldpassword cascade;
 
 alter table gacl.acllogin add column totp_key varchar;
 COMMENT ON COLUMN gacl.acllogin.totp_key IS E'TOTP secret key for the user';
@@ -9,7 +9,7 @@ CREATE UNIQUE INDEX acllogin_login_idx ON gacl.acllogin
 	);
 create sequence otolithe.dbparam_dbparam_id_seq;
 select setval( 'otolithe.dbparam_dbparam_id_seq', (select max(dbparam_id) from otolithe.dbparam));
-alter table otolithe.dbparam alter column dbparam_id set default nextval('metabo.dbparam_dbparam_id_seq');
+alter table otolithe.dbparam alter column dbparam_id set default nextval('otolithe.dbparam_dbparam_id_seq');
 insert into otolithe.dbparam (dbparam_name, dbparam_value) values ('otp_issuer', 'otolithe');
 
 insert into otolithe.dbversion (dbversion_number, dbversion_date) values ('2.4', '2022-08-05');
