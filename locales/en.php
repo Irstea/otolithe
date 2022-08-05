27bfbff004bff78effefde43efa5b5ce1ab1848f
Eric Quinton <eric.quinton@inrae.fr>
2022-08-05 16:52:02 +0200

    :fix news: correction de l'affichage des nouveautés

--- locales/en.php
+++ locales/en.php
@@ -9,4 +9,5 @@ $LANG["date"]["locale"] = "en";
 $LANG["date"]["formatdate"] = "MM-DD-YYYY";
 $LANG["date"]["formatdatetime"] = "MM-DD-YYYY HH:mm:ss";
 $LANG["date"]["formatdatecourt"] = "mm-dd-yy";
+
 ?>
