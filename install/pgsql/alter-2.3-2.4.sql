9d700744167fe72292b50ef858badee971c2c185
Eric Quinton <eric.quinton@inrae.fr>
2022-08-05 16:23:46 +0200

    :feat framework: suite mise à niveau

---
+++ install/pgsql/alter-2.3-2.4.sql
@@ -0,0 +1,13 @@
+drop table login_oldpassword cascade;
+
+alter table gacl.acllogin add column totp_key varchar;
+COMMENT ON COLUMN gacl.acllogin.totp_key IS E'TOTP secret key for the user';
+CREATE UNIQUE INDEX acllogin_login_idx ON gacl.acllogin
+	USING btree
+	(
+	  login
+	);
+create sequence otolithe.dbparam_dbparam_id_seq;
+select setval( 'otolithe.dbparam_dbparam_id_seq', (select max(dbparam_id) from otolithe.dbparam));
+alter table otolithe.dbparam alter column dbparam_id set default nextval('metabo.dbparam_dbparam_id_seq');
+insert into otolithe.dbparam (dbparam_name, dbparam_value) values ('otp_issuer', 'otolithe');
